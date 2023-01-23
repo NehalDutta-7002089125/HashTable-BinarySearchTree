ebfd5940d73579d16944b578b4bd3e829308074d
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2023-01-23 11:37:58 +0530

    [Nehal] Adding nodes in the BST

---
+++ UC-BST/BST.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC_BST
+{
+    internal class BST<T> where T : IComparable
+    {
+        public T data;
+        public BST<T> leftNode;
+        public BST<T> rightNode;
+        public BST(T data)
+        {
+            this.data = data;
+        }
+        int Leftcount=0, Rightcount=0;
+        public void add(T element)
+        {
+            T a= this.data;
+            if(a.CompareTo(element) > 0 )
+            {
+                if(leftNode==null)
+                {
+                    leftNode = new BST<T>(element);
+                }
+                else
+                {
+                    leftNode.add(element);
+                }
+            }
+            else
+            {
+                if(rightNode==null)
+                {
+                    rightNode= new BST<T>(element);
+                }
+                else
+                {
+                    rightNode.add(element);
+                }
+            }
+        }
+        public void show()
+        {
+            if(leftNode!=null)
+            {
+                leftNode.show();
+            }
+            Console.WriteLine(data.ToString());
+            if(rightNode!=null)
+            {
+                rightNode.show();
+            }
+        }
+    }
+}
---
+++ UC-BST/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UC_BST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BST<int> bst = new BST<int>(56);
+            bst.add(30);
+            bst.add(70);
+            bst.show();
+        }
+    }
+}
